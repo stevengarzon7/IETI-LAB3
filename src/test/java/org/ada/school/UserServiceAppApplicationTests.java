3c5a854b3a8c8c08b4f122b76fda3fe29a576c80
Steven Garzon Reina <stevengarzonreina@iMac-de-Steven.local>
2022-02-15 10:47:54 -0500

    inicializando repositorio

---
+++ src/test/java/org/ada/school/UserServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ada.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
